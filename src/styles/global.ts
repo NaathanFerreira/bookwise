71ce3721e677fda430303d38c30cd2dc1969f86d
Nathan Ferreira <nathan.ferreira@pca.com.br>
2023-06-15 10:43:42 -0300

    feat: initial CSS global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,14 @@
+import { globalCss } from '@stitches/react'
+
+export const globalStyles = globalCss({
+  '*': {
+    margin: 0,
+    padding: 0,
+    boxSizing: 'border-box',
+  },
+
+  'body, input, button': {
+    fontFamily: 'Nunito',
+    fontWeight: 400,
+  },
+})
---
+++ src/styles/pages/app.ts
@@ -0,0 +1,7 @@
+import { styled } from '..'
+
+export const Container = styled('div', {
+  minHeight: '100vh',
+  width: '100%',
+  background: '$gray800',
+})
