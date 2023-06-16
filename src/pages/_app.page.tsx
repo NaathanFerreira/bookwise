a94a26553079458e66c2db1d06552b5f402801e8
Nathan Ferreira <nathan.ferreira@pca.com.br>
2023-06-16 15:12:29 -0300

    feat: Google/Github authentications with next-auth

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,13 +1,19 @@
 import { globalStyles } from '@/styles/global'
 import { Container } from '@/styles/pages/app'
+import { SessionProvider } from 'next-auth/react'
 import { AppProps } from 'next/app'
 
 globalStyles()
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   return (
-    <Container>
-      <Component {...pageProps} />
-    </Container>
+    <SessionProvider session={session}>
+      <Container>
+        <Component {...pageProps} />
+      </Container>
+    </SessionProvider>
   )
 }
---
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -0,0 +1,47 @@
+import NextAuth, { NextAuthOptions } from 'next-auth'
+import GithubProvider from 'next-auth/providers/github'
+import GoogleProvider from 'next-auth/providers/google'
+export const authOptions: NextAuthOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID ?? '',
+      clientSecret: process.env.GITHUB_SECRET ?? '',
+      authorization: {
+        params: {
+          scope: 'read:user',
+        },
+      },
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+      authorization: {
+        params: {
+          // configuration to provide an refresh token
+          prompt: 'consent',
+          access_type: 'offline',
+          response_type: 'code',
+          scope:
+            'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile',
+        },
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account, user, profile, session, trigger }) {
+      if (account?.access_token && account.refresh_token) {
+        token.access_token = account.access_token
+        token.refresh_token = account.refresh_token
+      }
+      return token
+    },
+    async session({ session, token }) {
+      return {
+        ...session,
+        token,
+      }
+    },
+  },
+}
+export default NextAuth(authOptions)
