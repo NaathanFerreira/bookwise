a68968582b4431031d8b68ff1a590fa271029ed6
Nathan Ferreira <nathan.ferreira@pca.com.br>
2023-06-29 21:09:25 -0300

    feat: fetching books and filtering by categories in Explore Page

--- src/pages/explore/components/BookCard/index.tsx
+++ src/pages/explore/components/BookCard/index.tsx
@@ -1,28 +1,39 @@
 import { BookCardContainer, BookCardContent } from './styles'
-import BookImageExample from '../../../../assets/book-example2.png'
 import Image from 'next/image'
 import StarsRating from '@/components/StarsRating'
 import * as Dialog from '@radix-ui/react-dialog'
 import BookDetailsDrawer from '../BookDetailsDrawer'
 
-export default function BookCard() {
+interface BookCardProps {
+  coverImageUrl: string
+  title: string
+  author: string
+  rate: number
+}
+
+export default function BookCard({
+  coverImageUrl,
+  title,
+  author,
+  rate,
+}: BookCardProps) {
   return (
     <Dialog.Root>
       <Dialog.Trigger asChild>
         <BookCardContainer>
           <Image
             quality={100}
-            src={BookImageExample.src}
+            src={coverImageUrl}
             width={108}
             height={152}
             alt=" Book cover image "
           />
           <BookCardContent>
             <label>
-              <h1>A revolução dos bichos</h1>
-              <span>George Orwell</span>
+              <h1>{title}</h1>
+              <span>{author}</span>
             </label>
-            <StarsRating rate={2} />
+            <StarsRating rate={rate} />
           </BookCardContent>
         </BookCardContainer>
       </Dialog.Trigger>
--- src/pages/explore/index.page.tsx
+++ src/pages/explore/index.page.tsx
@@ -10,8 +10,61 @@ import {
 } from './styles'
 import { HiMagnifyingGlass } from 'react-icons/hi2'
 import BookCard from './components/BookCard'
+import { useQuery } from 'react-query'
+import { api } from '@/lib/axios'
+import { Category } from '@prisma/client'
+import { useState } from 'react'
+
+type BooksWithAvgRating = {
+  ratings: number
+  avgRating: number
+  alreadyRead: boolean
+  id: string
+  name: string
+  author: string
+  summary: string
+  cover_url: string
+  total_pages: number
+  created_at: Date
+}
+
+interface BooksApiReponse {
+  books: BooksWithAvgRating[]
+}
+
+interface BooksCategoriesApiResponse {
+  categories: Category[]
+}
 
 export default function Explore() {
+  const [selectedCategory, setSelectedCategory] = useState<string | null>(null)
+
+  const { data: books } = useQuery<BooksWithAvgRating[]>(
+    ['books', selectedCategory],
+    async () => {
+      const response = await api.get<BooksApiReponse>('/books', {
+        params: {
+          category: selectedCategory,
+        },
+      })
+      return response.data.books
+    },
+  )
+
+  const { data: categories } = useQuery<Category[]>(
+    ['categories'],
+    async () => {
+      const response = await api.get<BooksCategoriesApiResponse>(
+        '/books/categories',
+      )
+      return response.data.categories
+    },
+  )
+
+  function handleChangeCategory(categoryId: string | null) {
+    setSelectedCategory(categoryId)
+  }
+
   return (
     <DefaultLayout>
       <ExplorePageContainer>
@@ -25,32 +78,36 @@ export default function Explore() {
           </SearchInputContainer>
         </ExplorePageHeader>
         <FilterOptions>
-          <FilterItem selected>All</FilterItem>
-          <FilterItem>Tech</FilterItem>
-          <FilterItem>Education</FilterItem>
-          <FilterItem>Fantasy</FilterItem>
-          <FilterItem>Science Fiction</FilterItem>
-          <FilterItem>Horror</FilterItem>
-          <FilterItem>Heros</FilterItem>
-          <FilterItem>Suspense</FilterItem>
+          <FilterItem
+            selected={selectedCategory === null}
+            onClick={() => handleChangeCategory(null)}
+          >
+            All
+          </FilterItem>
+          {categories?.map((category) => {
+            return (
+              <FilterItem
+                selected={selectedCategory === category.id}
+                key={category.id}
+                onClick={() => handleChangeCategory(category.id)}
+              >
+                {category.name}
+              </FilterItem>
+            )
+          })}
         </FilterOptions>
         <BookList>
-          <BookCard />
-          <BookCard />
-          <BookCard />
-          <BookCard />
-          <BookCard />
-          <BookCard />
-          <BookCard />
-          <BookCard />
-          <BookCard />
-          <BookCard />
-          <BookCard />
-          <BookCard />
-          <BookCard />
-          <BookCard />
-          <BookCard />
-          <BookCard />
+          {books?.map((book) => {
+            return (
+              <BookCard
+                key={book.id}
+                coverImageUrl={book.cover_url}
+                title={book.name}
+                author={book.author}
+                rate={book.avgRating}
+              />
+            )
+          })}
         </BookList>
       </ExplorePageContainer>
     </DefaultLayout>
